63a21ffad694d889785265b14da144baf797e8bf
OTalDoL2 <lucas.ramos_dev@outlook.com>
2023-09-29 15:02:35 -0300

    adicionando imagens

--- header.php
+++ header.php
@@ -1,31 +1,3 @@
-<!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="#">.Coffee</a>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="sobre.php">Sobre</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contato</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav> -->
-
-
-
-
-
-
-
 <?php
     function statusPagina($pagina) {
         $classeRetorno = '';
@@ -44,20 +16,13 @@
     }
 ?>
 
-
-
-
-
-
-
-<!-- navbar.php -->
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary ">
   <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="#">.Coffee</a>
+      <a class="navbar-brand" href="#">.Coffee ☕</a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
             <a class="nav-link <?=statusPagina('/') ?>" href="index.php">Home</a>
--- index.php
+++ index.php
@@ -9,9 +9,32 @@
         <title> Dot Coffee </title>
         <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="./css/style.css">
+        <script src="./bootstrap/js/bootstrap.min.js"></script>
     </head>
     <body>
         <?php include('header.php')?>
-        <p>treste</p>
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="./assets/sf7lY6.jpg" class="d-block" alt="...">
+
+                </div>
+                <div class="carousel-item">
+                <img src="./assets/jk1QqU.jpg" class="d-block" alt="...">
+                
+                </div>
+                <div class="carousel-item">
+                <img src="./assets/thumb-1920-774741.jpg" class="d-block w-100" alt="imagem de uma maquina de café expresso e uma moedora de grãos elétrica">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </body>
 </html>
